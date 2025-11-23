eb8c25695223a7a2f37d5c463c6ab069716302cb
MohanYuan2005 <mohan.yuan@mail.utoronto.ca>
2025-11-22 20:20:23 -0500

    Task 1.1: fix CheckStyle issues

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -43,36 +43,7 @@ public class StatementPrinter {
         for (Performance p : invoice.getPerformances()) {
 
             final Play play = plays.get(p.getPlayID());
-            int thisAmount = 0;
-
-            // Calculate amount based on play type
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = Constants.TRAGEDY_BASE_AMOUNT;
-
-                    if (p.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                                * (p.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
-                    }
-                    break;
-
-                case "comedy":
-                    thisAmount = Constants.COMEDY_BASE_AMOUNT;
-
-                    if (p.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
-                                + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                                * (p.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
-                    }
-
-                    thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE
-                            * p.getAudience();
-                    break;
-
-                default:
-                    throw new RuntimeException(
-                            String.format("unknown type: %s", play.getType()));
-            }
+            final int thisAmount = amountFor(p, play);
 
             // Volume credits
             volumeCredits += Math.max(
@@ -100,5 +71,48 @@ public class StatementPrinter {
         result.append(String.format("You earned %s credits%n", volumeCredits));
 
         return result.toString();
+    }
+
+    /**
+     * Calculates the price of a given performance based on the play type.
+     *
+     * @param performance the performance information
+     * @param play        the play associated with the performance
+     * @return the calculated amount in cents
+     */
+    private int amountFor(Performance performance, Play play) {
+        int result;
+
+        switch (play.getType()) {
+            case "tragedy":
+                result = Constants.TRAGEDY_BASE_AMOUNT;
+
+                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience()
+                            - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                }
+                break;
+
+            case "comedy":
+                result = Constants.COMEDY_BASE_AMOUNT;
+
+                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience()
+                            - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                }
+
+                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE
+                        * performance.getAudience();
+                break;
+
+            default:
+                throw new RuntimeException(
+                        String.format("unknown type: %s", play.getType()));
+        }
+
+        return result;
     }
 }
